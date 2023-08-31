19a7e04b1662decd5da4949a4e25103f122052cf
shuuxdev <72917643+shuuxdev@users.noreply.github.com>
2023-08-31 22:40:37 +0700

    Add wwwroot to fix WebRootPath return null

---
+++ wwwroot/dist/static/js/276.d2ee5fd1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[276],{276:function(e,n,u){u.r(n),u.d(n,{default:function(){return c}});var r=u(1413),t=u(2791),f=u(7739);function c(e){var n=(0,t.useRef)(null),u=(0,t.useRef)(null);return u.current&&u.current.update(e),(0,t.useEffect)((function(){return u.current=new(0,f.cW)((0,r.Z)((0,r.Z)({},e),{},{ref:n})),function(){u.current=null}}),[]),t.createElement("div",{ref:n})}}}]);
+//# sourceMappingURL=276.d2ee5fd1.chunk.js.map
